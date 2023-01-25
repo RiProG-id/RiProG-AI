06f62bc5071a6a96926fbc7f8d4caeafce5d07bc
RiProG-id <81682107+RiProG-id@users.noreply.github.com>
2023-01-25 11:38:43 +0800

    Add files via upload

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,13 +1,36 @@
-#Permission
-[[ -f /data/adb/modules/RiProG/dummytonotification ]] && (
+[[ ! -f /data/adb/modules/RiProG/setupcomplete ]] && (
 chmod 755 /data/adb/modules/RiProG/script
-chmod 755 /data/adb/modules/RiProG/detector
-chmod 755 /data/adb/modules/RiProG/mode
-chmod 755 /data/adb/modules/RiProG/loop
+chmod 755 /data/adb/modules/RiProG/ai
 )
-
-#Debloat Unnessesary Things
-[[ -f /data/adb/modules/RiProG/dummytonotification ]] && (
+[[ ! -f /data/adb/modules/RiProG/setupcomplete ]] && (
+mkdir /data/adb/modules/RiProG/feature
+mkdir /data/adb/modules/RiProG/feature/ai
+mkdir /data/adb/modules/RiProG/feature/tweak
+echo "" > /data/adb/modules/RiProG/feature/ai/notification
+echo "" > /data/adb/modules/RiProG/feature/ai/powersave
+echo "" > /data/adb/modules/RiProG/feature/ai/lmk
+echo "" > /data/adb/modules/RiProG/feature/ai/maxfreq
+echo "" > /data/adb/modules/RiProG/feature/ai/halfcore
+echo "" > /data/adb/modules/RiProG/feature/tweak/debloat
+echo "" > /data/adb/modules/RiProG/feature/tweak/dalvikexec
+echo "" > /data/adb/modules/RiProG/feature/tweak/purgeassets
+echo "" > /data/adb/modules/RiProG/feature/tweak/skiagl
+echo "" > /data/adb/modules/RiProG/feature/tweak/zygotepreforking
+echo "" > /data/adb/modules/RiProG/feature/tweak/fusepasstrough
+echo "" > /data/adb/modules/RiProG/feature/tweak/faststreaming
+echo "" > /data/adb/modules/RiProG/feature/tweak/mediacodex
+echo "" > /data/adb/modules/RiProG/feature/tweak/smoothgui
+echo "" > /data/adb/modules/RiProG/feature/tweak/networktweak
+echo "" > /data/adb/modules/RiProG/feature/tweak/clearaudio
+echo "" > /data/adb/modules/RiProG/feature/tweak/disableloganddebug
+echo "" > /data/adb/modules/RiProG/feature/tweak/disableprink
+echo "" > /data/adb/modules/RiProG/feature/tweak/disablepanic
+echo "" > /data/adb/modules/RiProG/feature/tweak/fstrim
+)
+[[ ! -f /data/adb/modules/RiProG/setupcomplete ]] && (echo "" > /data/adb/modules/RiProG/setupcomplete)
+if [[ -f /data/adb/modules/RiProG/feature/tweak/debloat ]]
+then
+[[ ! -d /data/adb/modules/RiProG/system ]] && (
 mkdir /data/adb/modules/RiProG/system
 mkdir /data/adb/modules/RiProG/system/bin
 mkdir /data/adb/modules/RiProG/system/etc
@@ -160,6 +183,202 @@ echo "" > /data/adb/modules/RiProG/system/vendor/etc/init/vendor.sprd.hardware.c
 echo "" > /data/adb/modules/RiProG/system/vendor/etc/init/vendor.sprd.hardware.log@1.0-service.rc
 echo "" > /data/adb/modules/RiProG/system/vendor/etc/init/yloglite.rc
 )
+else
+[[ -d /data/adb/modules/RiProG/system ]] && (rm -rf /data/adb/modules/RiProG/system)
+fi
+[[ -f /data/adb/modules/RiProG/feature/tweak/dalvikexec ]] && (resetprop -n dalvik.vm.execution-mode int:jit)
+[[ -f /data/adb/modules/RiProG/feature/tweak/purgeassets ]] && (resetprop -n persist.sys.purgeable_assets 1)
+[[ -f /data/adb/modules/RiProG/feature/tweak/skiagl ]] && (resetprop -n debug.hwui.renderer skiagl)
+[[ -f /data/adb/modules/RiProG/feature/tweak/zygotepeforking ]] && (resetprop -n persist.device_config.runtime_native.usap_pool_enabled true)
+[[ -f /data/adb/modules/RiProG/feature/tweak/fusepasstrough ]] && (resetprop -n persist.sys.fuse.passthrough.enable true)
+#Fast Streaming
+[[ -f /data/adb/modules/RiProG/feature/tweak/faststreaming ]] && (
+resetprop -n media.stagefright.enable-player true
+resetprop -n media.stagefright.enable-meta true
+resetprop -n media.stagefright.enable-scan true
+resetprop -n media.stagefright.enable-http true
+resetprop -n media.stagefright.enable-rtsp true
+resetprop -n media.stagefright.enable-record false
+)
 
-#First run only
-[[ -f /data/adb/modules/RiProG/dummytonotification ]] && (mv /data/adb/modules/RiProG/dummytonotification /data/adb/modules/RiProG/notification)
+#Media Codex
+[[ -f /data/adb/modules/RiProG/feature/tweak/mediacodex ]] && (
+resetprop -n ro.media.dec.aud.flac.enabled 1
+resetprop -n ro.media.dec.aud.wma.enabled 1
+resetprop -n ro.media.dec.vid.avi.enabled 1
+resetprop -n ro.media.dec.vid.wmv.enabled 1
+resetprop -n ro.media.dec.aud.wma.enabled 1
+resetprop -n ro.media.dec.aud.mp3.enabled 1
+resetprop -n ro.media.dec.vid.mp4.enabled 1
+resetprop -n ro.media.dec.aud.flac.enabled 1
+resetprop -n ro.media.dec.vid.H.264.enabled 1
+resetprop -n ro.media.enc.aud.flac.enabled 1
+resetprop -n ro.media.enc.vid.H.264.enabled 1
+resetprop -n ro.media.enc.aud.wma.enabled 1
+resetprop -n ro.media.enc.vid.wmv.enabled 1
+resetprop -n ro.media.enc.aud.mp3.enabled 1
+resetprop -n ro.media.enc.vid.mp4.enabled 1
+)
+
+#Smooth GUI
+[[ -f /data/adb/modules/RiProG/feature/tweak/smoothgui ]] && (
+resetprop -n sys.use_fifo_ui 1
+resetprop -n windows.mgr.max_event_per_sec 180
+resetprop -n ro.min_pointer_dur 8
+resetprop -n ro.min.fling_velocity 8000
+resetprop -n ro.max.fling_velocity 12000
+resetprop -n persist.sys.lgospd.enable 0
+resetprop -n persist.sys.pcsync.enable 0
+resetprop -n persist.sys.scrollingcache 3
+)
+
+#Network Tweak
+[[ -f /data/adb/modules/RiProG/feature/tweak/networktweak ]] && (
+resetprop -n net.tcp.buffersize.default "6144,87380,110208,6144,16384,110208"
+resetprop -n net.tcp.buffersize.wifi 262144,524288,1048576,262144,524288,1048576"
+resetprop -n net.tcp.buffersize.lte "262144,524288,3145728,262144,524288,3145728"
+resetprop -n net.tcp.buffersize.hsdpa "6144,262144,1048576,6144,262144,1048576"
+resetprop -n net.tcp.buffersize.evdo_b "6144,262144,1048576,6144,262144,1048576"
+resetprop -n net.tcp.buffersize.umts "6144,87380,110208,6144,16384,110208"
+resetprop -n net.tcp.buffersize.hspa "6144,87380,262144,6144,16384,262144"
+resetprop -n net.tcp.buffersize.gprs "6144,8760,11680,6144,8760,11680"
+resetprop -n net.tcp.buffersize.edge "6144,26280,35040,6144,16384,35040"
+resetprop -n net.tcp.default_init_rwnd 60
+resetprop -n net.ipv4.tcp_ecn 0
+resetprop -n net.ipv4.route.flush 1
+resetprop -n net.ipv4.tcp_rfc1337 1
+resetprop -n net.ipv4.ip_no_pmtu_disc 0
+resetprop -n net.ipv4.tcp_sack 1
+resetprop -n net.ipv4.tcp_fack 1
+resetprop -n net.ipv4.tcp_window_scaling 1
+resetprop -n net.ipv4.tcp_timestamps 0
+resetprop -n net.ipv4.tcp_rmem "6144 87380 1048576"
+resetprop -n net.ipv4.tcp_wmem "6144 87380 1048576"
+resetprop -n net.ipv4.tcp_mem "187000 187000 187000"
+resetprop -n net.ipv4.tcp_no_metrics_save 1
+resetprop -n net.ipv4.icmp_echo_ignore_all 1
+resetprop -n net.ipv4.tcp_moderate_rcvbuf 1
+resetprop -n net.ipv4.conf.all.accept_redirects 0
+resetprop -n net.ipv4.conf.default.accept_redirects 0
+resetprop -n net.ipv4.conf.all.rp_filter 1
+resetprop -n persist.cust.tel.eons 1
+resetprop -n persist.cust.tel.adapt 1
+)
+[[ -f /data/adb/modules/RiProG/feature/tweak/clearaudio ]] && (
+resetprop -n persist.audio.fluence.mode endfire
+resetprop -n persist.audio.vr.enable true
+resetprop -n persist.audio.handset.mic digital
+resetprop -n af.resampler.quality 255
+resetprop -n mpq.audio.decode true
+)
+#Disable logger and debugger
+[[ -d /data/adb/modules/RiProG/feature/tweak/disableloganddebug ]] && (
+resetprop -n log.resolves 0
+resetprop -n log.pm4 0
+resetprop -n log.pm4mem 0
+resetprop -n log.shaders 0
+resetprop -n log.sc_dev 0
+resetprop -n log.cffdump 0
+resetprop -n log.cffdump_with_ifh 0
+resetprop -n log.cffdump_no_memzero 0
+resetprop -n log.dumpx 0 
+resetprop -n log.primitives 0
+resetprop -n log_frame_info 0
+resetprop -n log_mp4dectime 0
+resetprop -n log_posttime 0
+resetprop -n log_profile 0
+resetprop -n log_playerdriver 0
+resetprop -n log_metadatadriver 0
+resetprop -n log_playerengine 0
+resetprop -n log_datapath 0
+resetprop -n log_mp4parsernode 0
+resetprop -n log_basedecnode 0
+resetprop -n log_audiodecnode 0
+resetprop -n log_videodecnode 0
+resetprop -n log_outputnode 0
+resetprop -n log_outputnodeinputport 0
+resetprop -n log_audiooutput 0
+resetprop -n log_surfaceoutput 0
+resetprop -n log_omxmp4 0
+resetprop -n log_ao 0
+resetprop -n log_fps_interval 0
+resetprop -n debug_test 0
+resetprop -n profiler.debugmonitor false
+resetprop -n profiler.hung.dumpdobugreport false
+resetprop -n persist.android.strictmode 0
+resetprop -n dalvik.vm.verify-bytecode false
+resetprop -n libc.debug.malloc 0
+resetprop -n av.debug.disable.pers.cache true
+resetprop -n persist.vendor.ssr.enable_ramdumps 0
+resetprop -n config.disable_rtt true
+resetprop -n dalvik.vm.checkjni false
+resetprop -n db.log.slow_query_threshold 0
+resetprop -n debug.qualcomm.sns.hal 0 
+resetprop -n debug.qualcomm.sns.daemon 0 
+resetprop -n debug.qualcomm.sns.libsensor1 0
+resetprop -n debug.egl.profiler 0
+resetprop -n debug.enable.gamed 0
+resetprop -n debug.enable.wl_log 0
+resetprop -n debug.hwc.otf 0
+resetprop -n debug.hwc_dump_en 0
+resetprop -n debug.sf.dump 0
+resetprop -n debug.sf.ddms 0
+resetprop -n debug.sf.gpu_comp_tiling 0
+resetprop -n debug.sf.disable_backpressure 1
+resetprop -n debug.sf.recomputecrop 0
+resetprop -n debugtool.anrhistory 0
+resetprop -n hwui.use_gpu_pixel_buffers false
+resetprop -n logcat.live disable
+resetprop -n logcast.live disable
+resetprop -n live.logcat disable
+resetprop -n net.ipv4.tcp_no_metrics_save 1
+resetprop -n media.stagefright.log-uri 0
+resetprop -n profiler.debugmonitor false
+resetprop -n profiler.launch false
+resetprop -n profiler.hung.dumpdobugreport false
+resetprop -n persist.android.strictmode 0
+resetprop -n ro.egl.destroy_after_detach false
+resetprop -n ro.config.ksm.support false
+resetprop -n ro.config.nocheckin=1
+resetprop -n ro.debuggable=0
+resetprop -n ro.logd.size=0
+resetprop -n ro.compcache.default 0
+resetprop -n ro.kernel.android.checkjni 0
+resetprop -n ro.kernel.qemu.gles 0
+resetprop -n ro.kernel.checkjni 0
+resetprop -n ro.sf.battery_log 0
+resetprop -n ro.sf.battery.log.enabled 0
+resetprop -n ro.telephony.call_ring.multiple false
+resetprop -n rw.logger 0
+resetprop -n persist.ims.disableDebugLogs 1
+resetprop -n persist.debug.wfd.enable 0
+resetprop -n persist.bt.a2dp.aac_disable true
+resetprop -n persist.data.qmi.adb_logmask 0
+resetprop -n persist.debug.sensors.hal 0 
+resetprop -n persist.data.qmi.adb_logmask 0
+resetprop -n persist.brcm.log none 
+resetprop -n persist.brcm.cp_crash none 
+resetprop -n persist.brcm.ap_crash none
+resetprop -n persist.sys.dun.override 0
+resetprop -n persist.radio.oem_socket false
+resetprop -n persist.sys.wfd.virtual 0
+resetprop -n persist.ims.enableADBLogs 0
+resetprop -n persist.ims.enableDebugLogs 0
+resetprop -n persist.ims.disabled 1
+resetprop -n persist.ims.disableQXDMLogs 0
+resetprop -n persist.ims.disableIMSLogs 0
+resetprop -n persist.sys.offlinelog.kernel 1
+resetprop -n persist.sys.offlinelog.logcat 1
+resetprop -n persist.oem.dump 0
+resetprop -n profiler.forse_disable_err_rpt 1
+resetprop -n profiler.forse_disable_ulog=1
+resetprop -n sdm.debug.disable_inline_rotator 1
+resetprop -n sdm.debug.disable_skip_validate 1
+resetprop -n sys.games.gt.prof 1
+resetprop -n sys.hwc.gpu_perf_mode 0
+resetprop -n vendor.fm.a2dp.conc.disabled true
+resetprop -n vendor.vidc.enc.disable_bframes 1
+resetprop -n vidc.debug.level 0
+resetprop -n video.disable.ubwc 1
+resetprop -n wifi.interface wlan 0
+)
--- service.sh
+++ service.sh
@@ -1,4 +1,7 @@
 sleep 30
-su -c /data/adb/modules/RiProG/script
-su -c /data/adb/modules/RiProG/loop
-
+[[ -d /data/adb/modules/RiProG/feature/tweak ]] && (su -c /data/adb/modules/RiProG/tweak)
+[[ -d /data/adb/modules/RiProG/feature/ai ]] && (
+resetprop -n tweak.detect.mode unset
+resetprop -n tweak.apply.mode unset
+su -c /data/adb/modules/RiProG/ai
+)

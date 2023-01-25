6c2ec5e2ecdef9603414bbbb7d3f282c0ac3c5d3
RiProG-id <81682107+RiProG-id@users.noreply.github.com>
2023-01-25 12:36:21 +0800

    Add files via upload

--- service.sh
+++ service.sh
@@ -1,5 +1,5 @@
 sleep 30
-[[ -d /data/adb/modules/RiProG/feature/tweak ]] && (su -c /data/adb/modules/RiProG/tweak)
+[[ -d /data/adb/modules/RiProG/feature/tweak ]] && (su -c /data/adb/modules/RiProG/tweak2)
 [[ -d /data/adb/modules/RiProG/feature/ai ]] && (
 resetprop -n tweak.detect.mode unset
 resetprop -n tweak.apply.mode unset

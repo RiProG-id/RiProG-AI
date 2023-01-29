53a271727712760830400b7a512c11da0688f87c
RiProG-id <81682107+RiProG-id@users.noreply.github.com>
2023-01-30 06:22:28 +0800

    Add files via upload

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,1 +1,6 @@
+[[ -f /data/adb/modules/RiProG/setup ]] && (
+chmod 755  /data/adb/modules/RiProG/setup
+/data/adb/modules/RiProG/setup
+rm /data/adb/modules/RiProG/setup
+)
 [[ -d /data/adb/modules/RiProG/feature/tweak ]] && (su -c /data/adb/modules/RiProG/pretweak)

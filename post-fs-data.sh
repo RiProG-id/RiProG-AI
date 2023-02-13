09cbf154d0e4631960f1fdf3fc3ba7e6c29fbf6f
RiProG-id <81682107+RiProG-id@users.noreply.github.com>
2023-02-13 19:32:09 +0800

    Add files via upload

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,6 +1,1 @@
-[[ -f /data/adb/modules/RiProG/setup ]] && (
-chmod 755  /data/adb/modules/RiProG/setup
-/data/adb/modules/RiProG/setup
-rm /data/adb/modules/RiProG/setup
-)
-[[ -d /data/adb/modules/RiProG/feature/tweak ]] && (su -c /data/adb/modules/RiProG/pretweak)
+su -c "${0%/*}/RiProG debug0 postfsdata"
--- service.sh
+++ service.sh
@@ -1,3 +1,1 @@
-while [[ -z $(getprop sys.boot_completed) ]]; do sleep 5; done 
-[[ -d /data/adb/modules/RiProG/feature/tweak ]] && (su -c /data/adb/modules/RiProG/script/sincetweak)
-[[ -d /data/adb/modules/RiProG/feature/ai ]] && (su -c /data/adb/modules/RiProG/script/preai)
+su -c "${0%/*}/RiProG debug0 service"
